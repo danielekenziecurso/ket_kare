a34e9b191169f602af5d628525f190f6fd183850
unknown <danieleccb25@hotmail.com>
2023-08-07 08:37:05 -0300

    feat: second task finished

--- pets/views.py
+++ pets/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from rest_framework.views import status, Request, Response, APIView
+from .models import Pet
+from groups.models import Group
+from traits.models import Trait
+from .serializers import PetSerializer
+from rest_framework.pagination import PageNumberPagination
 
-# Create your views here.
+
+class PetView(APIView, PageNumberPagination):
+    def post(self, request: Request) -> Response:
+        serializer = PetSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        receive_group = serializer.validated_data.pop("group")
+        receive_traits = serializer.validated_data.pop("traits")
+        group = Group.objects.filter(
+            scientific_name__iexact=receive_group["scientific_name"]
+        ).first()
+        pet = None
+        if not group:
+            group = Group.objects.create(**receive_group)
+            pet = Pet.objects.create(**serializer.validated_data, group=group)
+            for trait_data in receive_traits:
+                trait = Trait.objects.filter(name__iexact=trait_data["name"]).first()
+                if not trait:
+                    trait = Trait.objects.create(**trait_data)
+                trait.pets.add(pet)
+
+        serializer = PetSerializer(pet)
+
+        return Response(serializer.data, status.HTTP_201_CREATED)
+
+    def get(self, request: Request) -> Response:
+        traits = request.query_params.get("trait")
+        pets = Pet.objects.all()
+        if traits:
+            filtered_traits = Pet.objects.filter(trait_name=traits).all()
+            result = self.paginate_queryset(filtered_traits, request)
+        else:
+            result = self.paginate_queryset(pets, request)
+        serializer = PetSerializer(result, many=True)
+        return self.get_paginated_response(serializer.data)
--- traits/models.py
+++ traits/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Trait(models.Model):
     name = models.CharField(max_length=20, unique=True)
-    # pet = models.ManyToManyField("pets.Pet")
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __repr__(self):
         return f"<[{self.pk}] {self.name}>"
