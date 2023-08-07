ac3da951b99756f3de1c2ace4b8051320fb56b8b
unknown <danieleccb25@hotmail.com>
2023-08-07 16:41:44 -0300

    feat: 4 task finished

--- pets/urls.py
+++ pets/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [path("pets/", views.PetView.as_view())]
+urlpatterns = [
+    path("pets/", views.PetView.as_view()),
+    path("pets/<int:pet_id>/", views.PetDetailVieaw.as_view()),
+]
--- pets/views.py
+++ pets/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.views import status, Request, Response, APIView
 from .models import Pet
 from groups.models import Group
@@ -22,21 +23,61 @@ class PetView(APIView, PageNumberPagination):
             trait = Trait.objects.filter(name__iexact=traits["name"]).first()
             if not trait:
                 trait = Trait.objects.create(**traits)
-        
+
             pet.traits.add(trait)
 
-     
         serializer = PetSerializer(pet)
 
         return Response(serializer.data, status.HTTP_201_CREATED)
 
     def get(self, request: Request) -> Response:
         traits = request.query_params.get("trait")
-        pets = Pet.objects.all()
+
         if traits:
-            filtered_traits = Pet.objects.filter(trait_name=traits).all()
-            result = self.paginate_queryset(filtered_traits, request)
+            pets = Pet.objects.filter(traits__name=traits).all()
         else:
-            result = self.paginate_queryset(pets, request)
+            pets = Pet.objects.all()
+        result = self.paginate_queryset(pets, request)
         serializer = PetSerializer(result, many=True)
         return self.get_paginated_response(serializer.data)
+
+
+class PetDetailVieaw(APIView):
+    def get(self, request: Request, pet_id: int) -> Response:
+        pet = get_object_or_404(Pet, pk=pet_id)
+
+        serializer = PetSerializer(pet)
+
+        return Response(serializer.data)
+
+    def patch(self, request: Request, pet_id: int) -> Response:
+        pet = get_object_or_404(Pet, pk=pet_id)
+        serializer = PetSerializer(instance=pet, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        receive_group = serializer.validated_data.pop("group", None)
+        receive_traits = serializer.validated_data.pop("traits", None)
+        if receive_group:
+            group = Group.objects.filter(
+                scientific_name__iexact=receive_group["scientific_name"]
+            ).first()
+            if not group:
+                group = Group.objects.create(**receive_group)
+            pet.group = group
+        if receive_traits:
+            traits_to_add = []
+            for trait_data in receive_traits:
+                trait = Trait.objects.filter(name__iexact=trait_data["name"]).first()
+                if not trait:
+                    trait = Trait.objects.create(**trait_data)
+                traits_to_add.append(trait)
+            pet.traits.set(traits_to_add)
+        for key, value in serializer.validated_data.items():
+            setattr(pet, key, value)
+        pet.save()
+        serializer = PetSerializer(pet)
+        return Response(serializer.data)
+
+    def delete(self, request: Request, pet_id: int) -> Response:
+        pet = get_object_or_404(Pet, pk=pet_id)
+        pet.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
